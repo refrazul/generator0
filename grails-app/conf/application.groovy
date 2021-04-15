6462bdbeacaa00d4026d221aaf867a5e9613a744
refrazul <refrazul@gmail.com>
2021-04-15 15:16:00 -0500

    Se agrega capa de seguridad

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,31 @@
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'org.palina.security.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.palina.security.UserRole'
+grails.plugin.springsecurity.authority.className = 'org.palina.security.Role'
+grails.plugin.springsecurity.logout.postOnly = false
+
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
+
---
+++ src/test/groovy/org/palina/security/RegisterControllerSpec.groovy
@@ -0,0 +1,18 @@
+package org.palina.security
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class RegisterControllerSpec extends Specification implements ControllerUnitTest<RegisterController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
